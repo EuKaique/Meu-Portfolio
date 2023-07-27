01ec11f1bac26802a58a6bb9ccd9559af3dce8ef
Kaique <kaiqueols75@gmail.com>
2023-07-27 15:35:40 -0300

    Update ImageSection.js

--- src/Components/ImageSection.js
+++ src/Components/ImageSection.js
@@ -12,16 +12,6 @@ function ImageSection() {
                 <p className="about-text">
                    Trabalho com tecnologia da informação desde 2018. Atuei inicialmente como analista de sistemas, realizando a tradução de um banco de dados Oracle PL/SQL. No ano seguinte trabalhei brevemente como Técnico de informática, mas não me adequei a posição. Em Agosto de 2021 comecei a trabalhar como desenvolvedor full-stack PHP, foi um período bem desafiador, pois fiquei trabalhando sozinho como desenvolvedor por 4 meses, até contratarem outra pessoa para me ajudar. Em Julho de 2022 fui integrado a DuoSystem, empresa que estou atualmente, e que deu um UP na minha carreira, pois estou trabalhando com as tecnologias de ponta do mercado no desenvolvimento de software.
                 </p>
-                <div className="about-details">
-                    <div className="left-section">
-                        <p>Nome Completo</p>
-                        <p>Idade</p>
-                    </div>
-                    <div className="right-section">
-                        <p>: Kaique Oliveira Santos</p>
-                        <p>: 27 anos</p>
-                    </div>
-                </div>
             </div>
         </div>
     )
