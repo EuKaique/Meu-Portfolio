eea9bd7eea8718a723bd48919755fd99688c0fcf
Kaique <kaiqueols75@gmail.com>
2023-08-02 10:57:13 -0300

    Update MenuItems.js

--- src/Components/MenuItems.js
+++ src/Components/MenuItems.js
@@ -7,12 +7,9 @@ function MenuItems({menuItem}) {
                 menuItem.map((item)=>{
                     return <div className="portfolio" key={item.id}>
                         <div className="image-data">
-                            <img src={item.image} alt=""/>
-                            <ul className="hover-items">
-                                <li>
-                                    <a href={item.link1} target="_blank" rel="noreferrer">{item.icon1}</a>
-                                </li>
-                            </ul>
+                            <a href={item.link1} target="_blank" rel="noreferrer">
+                                <img src={item.image} alt={item.image} />
+                            </a>
                         </div>
                         <h5>
                             {item.title}
